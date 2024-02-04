b7a230225ff7494474482b62af0edfa3b32fd57c
Potrunks <potrunks@hotmail.com>
2024-02-04 16:56:08 +0100

    Mise en place script de suivi de la cible #6

---
+++ Assets/Sources/Business/Implementation/TransformBusiness.cs
@@ -0,0 +1,30 @@
+﻿using Assets.Sources.Business.Interface;
+using Assets.Sources.Shared.Dtos;
+using UnityEngine;
+
+namespace Assets.Sources.Business.Implementation
+{
+    public class TransformBusiness : ITransformBusiness
+    {
+        public TransformBusiness() { }
+
+        public TransformDto CalculateTransformRelativeToTarget(Transform target, Vector3 offsetPosition, Vector3? offsetDegreesRotation)
+        {
+            return new TransformDto
+                (
+                    new Vector3
+                    (
+                        target.position.x + offsetPosition.x,
+                        target.position.y + offsetPosition.y,
+                        target.position.z + offsetPosition.z
+                    ),
+                    offsetDegreesRotation == null ? null : new Vector3
+                    (
+                        target.eulerAngles.x + offsetDegreesRotation.Value.x,
+                        target.eulerAngles.y + offsetDegreesRotation.Value.y,
+                        target.eulerAngles.z + offsetDegreesRotation.Value.z
+                    )
+                );
+        }
+    }
+}
