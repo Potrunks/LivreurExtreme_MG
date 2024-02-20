316d51084ff1d4673508fc8351faad62d29b1bad
Potrunks <potrunks@hotmail.com>
2024-02-21 00:11:46 +0100

    Suivi de la route qu'il reste a faire #16

---
+++ Assets/Sources/Components/ArrivalCheckpointSystem.cs
@@ -0,0 +1,20 @@
+﻿using Assets.Sources.Resources;
+using Assets.Sources.Shared.ScriptableObjects;
+using UnityEngine;
+
+namespace Assets.Sources.Components
+{
+    public class ArrivalCheckpointSystem : MonoBehaviour
+    {
+        [field: SerializeField]
+        public GameEvent ArrivalGameEvent { get; private set; }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.tag == TagResources.PLAYER)
+            {
+                ArrivalGameEvent.Raise();
+            }
+        }
+    }
+}
