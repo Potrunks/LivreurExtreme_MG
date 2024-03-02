0a12e2c5393700619a26c97e4417184cbee57a6a
Potrunks <potrunks@hotmail.com>
2024-03-02 01:10:54 +0100

    Mise en place proximity detector #22

---
+++ Assets/Sources/Components/ProximityDetectorSystem.cs
@@ -0,0 +1,32 @@
+﻿using Assets.Sources.Resources;
+using UnityEngine;
+
+namespace Assets.Sources.Components
+{
+    public class ProximityDetectorSystem : MonoBehaviour
+    {
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.tag == TagResources.OBSTACLE)
+            {
+                AutoMoveSystem autoMoveSystemHit = other.GetComponentInParent<AutoMoveSystem>();
+                if (autoMoveSystemHit != null)
+                {
+                    autoMoveSystemHit.Stop().Start();
+                }
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.tag == TagResources.OBSTACLE)
+            {
+                AutoMoveSystem autoMoveSystemHit = other.GetComponentInParent<AutoMoveSystem>();
+                if (autoMoveSystemHit != null)
+                {
+                    autoMoveSystemHit.GoForward().Start();
+                }
+            }
+        }
+    }
+}
